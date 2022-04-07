85d3c28a8af5ff02794cea14cc69917253015fb3
abdulhalimmunna <ahm2info@gmail.com>
2022-04-07 23:34:24 +0600

    commit from munna

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Library Management System</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	    <!-- Meta, title, CSS, favicons, etc. -->
+	    <meta charset="utf-8">
+	    <meta name="keyword" content="lms,library,library management,library management system,online library,library system,school library,management system,polytechnic library">
+		<meta name="description" content="“LIBRARY MANAGEMENT SYSTEM” is targeted towards the management of a modern library. All the book details are provided through an Online Public Access Catalogue visible throughout the local internet. Proper management and audit of books is possible through an easy to use interface for the library staff. Throughout the project the focus has been on presenting information and comments in an easy and intelligible manner.">
+		<meta name="author" content="Abdul Halim Munna">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="index/css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link rel="stylesheet" href="index/css/responsiveslides.css">
+		<link href="index/images/icon.ico" rel="icon"/>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="index/js/responsiveslides.min.js"></script>
+		  <script>
+		    // You can also use "$(window).load(function() {"
+			    $(function () {
+			
+			      // Slideshow 1
+			      $("#slider1").responsiveSlides({
+			        maxwidth: 1600,
+			        speed: 400
+			      });
+				});
+		  </script>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.php" style="font-family: Lucida Console; font-size: 30px; color: #73879C">Library Management system</a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						
+						<li><a href="student/login.php">Login</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		<div class="clear"> </div>
+			<!--start-image-slider---->
+					<div class="image-slider">
+						<!-- Slideshow 1 -->
+					    <ul class="rslides" id="slider1">
+					      <li><img src="index/images/2.jpg" alt=""></li>
+					      <li><img src="index/images/1.jpg" alt=""></li>
+					      <li><img src="index/images/3.jpg" alt=""></li>
+					      <li><img src="index/images/4.jpg" alt=""></li>
+					    </ul>
+						 <!-- Slideshow 2 -->
+					</div>
+					<!--End-image-slider---->
+		    <div class="clear"> </div>
+		    <div class="content-grids">
+		    	<div class="wrap">
+		    	<div class="section group">
+								
+							
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_1_of_2">
+						  <img src="index/images/students.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Students</h3>
+						  <p>Register & Login</p>
+						  <div class="button"><span><a href="student/login.php">Click Here</a></span></div>
+				    </div>
+				</div>	
+
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_1_of_2">
+						  <img src="index/images/librarian.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Librarian Login</h3>
+						
+						  <div class="button"><span><a href="librarian/login.php">Click Here</a></span></div>
+					</div>
+				</div>
+			</div>
+		    </div>
+		   </div>
+		   <div class="wrap">
+		   <div class="content-box">
+		   <div class="section group">
+				<div class="col_1_of_3 span_1_of_3 frist">
+				
+				</div>
+			</div>
+		   </div>
+		   </div>
+		   <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.php">Back To Top</a></li>
+					</ul>
+		   	</div>
+		   
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
